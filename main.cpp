e25ddefee2c43a0600dbe476dd132a7e9565c0e7
Kaan Günay <kaangunay16@gmail.com>
2020-09-02 14:30:27 +0300

    Threads id

--- main.cpp
+++ main.cpp
@@ -16,7 +16,12 @@ public:
 
 int main() {
     std::string msg = "Test";
+
+    std::cout << std::this_thread::get_id() << std::endl;  // Parent thread id
+
     std::thread t1((F()), msg);  // MOST VEXING SYNTAX
+    std::cout << t1.get_id() << std::endl;
+
     std::thread t2 = std::move(t1);
 
     t2.join();
